cef34ae740b8d037c154369ad25c3a18b4482d96
Alexey <alex.esprit@gmail.com>
2023-04-28 17:07:09 +0200

    Remove unused code

--- src/BrexEnvironment.ts
+++ src/BrexEnvironment.ts
@@ -7,23 +7,10 @@ export function getBaseUrl(env: BrexEnvironment): string {
   return baseUrlMap[env]
 }
 
-export function getAuthTokenUrl(env: BrexEnvironment): string {
-  return authTokenUrlMap[env]
-}
-
 const productionBaseUrl = 'https://platform.brexapis.com'
 const stagingBaseUrl = 'https://platform.staging.brexapps.com'
-
-const productionAuthTokenUrl = 'https://accounts.brex.com/oauth2/v1/token'
-const stagingAuthTokenUrl =
-  'https://accounts.staging.brexapps.com/oauth2/v1/token'
 
 const baseUrlMap: Record<BrexEnvironment, string> = {
   [BrexEnvironment.Production]: productionBaseUrl,
   [BrexEnvironment.Staging]: stagingBaseUrl,
-}
-
-const authTokenUrlMap: Record<BrexEnvironment, string> = {
-  [BrexEnvironment.Production]: productionAuthTokenUrl,
-  [BrexEnvironment.Staging]: stagingAuthTokenUrl,
 }
